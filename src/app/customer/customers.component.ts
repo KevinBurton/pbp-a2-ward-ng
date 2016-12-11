e5d3998a16b5faf060b567790091147e2b7b2127
Kevin Burton <rkevinburton@charter.net>
2016-12-11 07:25:26 -0600

    Changed scope of Customers Service

--- src/app/customer/customers.component.ts
+++ src/app/customer/customers.component.ts
@@ -5,8 +5,7 @@ import { CustomerService } from './customer.service';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css'],
-  providers: [CustomerService]
+  styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
   customers: any;
